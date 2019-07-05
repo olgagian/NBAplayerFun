3621c780b7bf45508a4c67066912bc404c96995b
Ioannis <olgagian@otenet.gr>
2019-07-05 19:31:29 +0300

    Project creation

---
+++ NBAplayerFun/NBAplayerFun/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  NBAplayerFun
+//
+//  Created by ioannis giannakidis on 05/07/2019.
+//  Copyright © 2019 ioannis giannakidis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
